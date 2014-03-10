5453cd4b48c2b8b6277fc1e0c403c0529ff7a9ab
Mayur Bindal <mbindal@mbindal-mbpro.local>
2014-03-09 23:56:04 -0700

    mainapplication

---
+++ NSX_app.py
@@ -0,0 +1,27 @@
+import inventory as mysets
+from neutronclient.v2_0 import client
+import novaclient.v1_1.client as nvclient
+from credentials import get_keystone_creds,get_nova_creds
+from sec_groups import sec_groups
+from network_subnet import network_subnet
+from Load_Balancer_floating_ip import  Load_Balancer_floating_ip
+
+def create_topology():
+    quantum_creds= get_keystone_creds()
+    quantum = client.Client(**quantum_creds)
+    nova_creds = get_nova_creds()
+    nova = nvclient.Client(**nova_creds)
+    network_object=network_subnet(quantum)
+    network_object.Create_network()
+    network_object.Create_subnet()
+    #enter the nova code to boot instances
+    security_grp_object=sec_groups(quantum)
+    security_grp_object.Create_securitygroup()
+    security_grp_object.Create_securitygroupules()
+    load_balancer=Load_Balancer_floating_ip(mysets.network_id,mysets.subnet_mapping,mysets.sec_id,quantum);
+    load_balancer.Create_Loadbalancers()
+    
+    
+    
+if __name__ == "__main__":
+    create_topology()
