ef4116275ae4190c5986fa0008761b3435d819b6
LonghaoShu <longhaoshu@gmail.com>
2014-03-10 17:13:06 -0700

    Create sec_port_update.py

---
+++ sec_port_update.py
@@ -0,0 +1,9 @@
+import inventory as myset
+
+class sec_port_update(object):
+    def __init__(self, quantum):
+            self.i=self.j=0;
+            self.quantum=quantum
+            
+    def Update_port(self, name):
+        
