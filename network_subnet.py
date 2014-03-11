3e37339d980bf84bcd96e15d259452b192a1d7b9
Mayur Bindal <mbindal@mbindal-mbpro.local>
2014-03-11 16:16:49 -0700

    changes to network...router addition

--- network_subnet.py
+++ network_subnet.py
@@ -5,11 +5,16 @@ class network_subnet(object):
             self.quantum=quantum
     #creation of pools and pool members
     def Create_network(self):
-        mysets.network_id=self.quantum.create_network(mysets.network_json)['network']['id']
-        mysets.private_network_id=self.quantum.create_network(mysets.network_json_private)['network']['id']
+        mysets.network_id=self.quantum.list_networks()['networks'][0]['id']
+        mysets.private_network_id=self.quantum.create_network(mysets.network_json_private)['networks']
+        mysets.router_id=self.quantum.list_routers()['routers'][0]['id']
     def Create_subnet(self):
+        i=0;
         for subnets in mysets.subnet_json['subnets']:
-            subnets['network_id']=mysets.private_network_id
+            subnets['network_id']=mysets.private_network_id[i]['id']
+            ++i
         subnet_list=self.quantum.create_subnet(mysets.subnet_json)['subnets']
         for ind_subnets in subnet_list:
             mysets.subnet_mapping[ind_subnets['name']]=ind_subnets['id']
+            body={'subnet_id': ind_subnets['id']}
+            self.quantum.add_interface_router(mysets.router_id,body)
